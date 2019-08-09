06abb5c3f87ac5e18bae79c4f7ab7c95d3abcfda
janesessive <eugene.absessiveabs@gmail.com>
2019-08-09 13:04:48 +0300

    checkbox

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,107 +1,140 @@
-import React, { Component } from 'react';
-import DatePicker from "react-datepicker"; 
-import panchang from '../../lib/panchang';
-import PanchangaInfo from '../PanchangaInfo/PanchangaInfo';
+import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import panchang from "../../lib/panchang";
+import PanchangaInfo from "../PanchangaInfo/PanchangaInfo";
 import "react-datepicker/dist/react-datepicker.css";
-import { dataResult } from '../../lib/dataResult';
-
+import { dataResult } from "../../lib/dataResult";
 
 class UserForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          startDate: new Date(),
-          timeZone: null,
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate: new Date(),
+      birthDate: new Date(),
+      timeZone: null
+    };
+    this.handleChangeCurrentDate = this.handleChangeCurrentDate.bind(this);
+    this.handleChangeBirthDate = this.handleChangeBirthDate.bind(this);
+  }
 
-        };
-        this.handleChange = this.handleChange.bind(this);
-      }
-     
-      handleChange(date) {
-        this.setState({
-          startDate: date
-        });
-      }
+  handleChangeCurrentDate(date) {
+    this.setState({
+      currentDate: date
+    });
+  }
 
-      onTimeZoneChanged = e => {
-        const zoneNumber = e.target.value;
-        this.setState({ timeZone: zoneNumber });
-      };
+  handleChangeBirthDate(date) {
+    this.setState({
+      birthDate: date
+    });
+  }
 
-      calculateResult = () => {
+  onTimeZoneChanged = e => {
+    const zoneNumber = e.target.value;
+    this.setState({ timeZone: zoneNumber });
+  };
 
-        let birthDate = this.state.startDate;
-        const birthInfo = panchang.calculate(birthDate);
-        let bInfo = {...birthInfo};
-        this.setState({birthInfo: bInfo });
-        
-const currentDate = new Date();
-        const currentInfo = panchang.calculate(currentDate);
-        this.setState({currentInfo, currentDate});
+  calculateResult = () => {
+    let birthDate = this.state.birthDate;
+    const birthInfo = panchang.calculate(birthDate);
+    let bInfo = { ...birthInfo };
+    this.setState({ birthInfo: bInfo });
 
-        let houseNumber = currentInfo.Raasi.index - birthInfo.Raasi.index;
-        if (houseNumber < 0) {
-            houseNumber = houseNumber + 12;
-        }
+    let currentDate = this.state.currentDate;
+    const currentInfo = panchang.calculate(currentDate);
+    this.setState({ currentInfo, currentDate });
 
-        houseNumber++;
-        this.setState({houseNumber});
+    let houseNumber = currentInfo.Raasi.index - birthInfo.Raasi.index;
+    if (houseNumber < 0) {
+      houseNumber = houseNumber + 12;
+    }
 
-        const description =dataResult.find(d=>d.id === houseNumber);
-        this.setState({description})
+    houseNumber++;
+    this.setState({ houseNumber });
 
+    const description = dataResult.find(d => d.id === houseNumber);
+    this.setState({ description });
+  };
 
-
-      }
-
-      render() {
-        return (
+  render() {
+    return (
+      <div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="currentDate">Дата и время Рождения: </label>
             <div>
-            <form>
+              <DatePicker
+                className="form-control"
+                selected={this.state.currentDate}
+                onChange={this.handleChangeCurrentDate}
+              />
+            </div>
+          </div>
           <div className="form-group">
             <label htmlFor="birthDate">Дата и время Рождения: </label>
-          <div>
+            <div>
               <DatePicker
-              className="form-control"
-                selected={this.state.startDate}
-                onChange={this.handleChange}
+                className="form-control"
+                selected={this.state.birthDate}
+                onChange={this.handleChangeBirthDate}
                 showTimeSelect
                 timeFormat="HH:mm"
                 timeIntervals={15}
                 dateFormat="MMMM d, yyyy h:mm aa"
                 timeCaption="time"
               />
-              </div>
+            </div>
           </div>
           <div className="form-group">
             <label htmlFor="timeZone">Часовой пояс: </label>
             <input
-            className="form-control"
               className="form-control"
               id="timeZone"
               type="number"
               onChange={this.onTimeZoneChanged}
               value={this.state.timeZone}
             />
-          </div>
-          </form>
-          <button 
-          type="button" 
-          class="btn btn-primary" 
-          onClick={this.calculateResult}
-          >Calculate</button>
-          <div>
-              <PanchangaInfo info={this.state.birthInfo} currentDate={this.state.startDate}/>
-              <PanchangaInfo info={this.state.currentInfo} currentDate={this.state.currentDate}/>
 
+            <label>
+              <input type="checkbox" value="west" name="zone" id="zone_west" />
+              West
+            </label>
+            <label>
+              <input type="checkbox" value="east" name="zone" id="zone_east" />
+              East
+            </label>
           </div>
+        </form>
+        <button
+          type="button"
+          class="btn btn-primary"
+          onClick={this.calculateResult}
+        >
+          Calculate
+        </button>
+        <div>
+          <PanchangaInfo
+            info={this.state.birthInfo}
+            currentDate={this.state.birthDate}
+          />
+          <PanchangaInfo
+            info={this.state.currentInfo}
+            currentDate={this.state.currentDate}
+          />
+        </div>
+        {this.state.houseNumber ? (
           <h2>Лунный дом: {this.state.houseNumber}</h2>
-        
-          <h3>{this.state.description? this.state.description.title : ""}</h3>
-          <p>{this.state.description? this.state.description.description : ""}</p>
-          </div>
-        );
-      }
+        ) : (
+          ""
+        )}
+
+        <h3>{this.state.description ? this.state.description.title : ""}</h3>
+        <p>
+          {this.state.description ? this.state.description.description : ""}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default UserForm;
