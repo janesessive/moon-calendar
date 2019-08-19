3ba3c4d96833ce2d26e5d717cb85cd331afdb36c
janesessive <eugene.absessiveabs@gmail.com>
2019-08-19 15:40:26 +0300

    transitInfo Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Panchanga from './components/Panchanga/Panchanga';
-import UserForm from './components/UserForm/UserForm';
+import { BrowserRouter } from 'react-router-dom';
+// import Panchanga from './components/Panchanga/Panchanga';
+// import UserForm from './components/UserForm/UserForm';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     {/* <Panchanga /> */}
-     <UserForm />
+     <Layout />
+     {/* <UserForm /> */}
     </div>
+    </ BrowserRouter>
   );
 }
 
