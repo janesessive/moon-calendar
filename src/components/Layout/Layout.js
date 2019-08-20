74b55a387f891ff9b94c6ddf8a5da50fb74ae029
janesessive <eugene.absessiveabs@gmail.com>
2019-08-20 14:11:33 +0300

    Switch Rout and Navbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Route } from 'react-router-dom';
-import './Layout.css';
-import UserForm from '../UserForm/UserForm';
-import TransitInfo from '../TransitInfo/TransitInfo';
-
-
-
+import { Route, Link, Switch } from "react-router-dom";
+import "./Layout.css";
+import UserForm from "../UserForm/UserForm";
+import TransitInfo from "../TransitInfo/TransitInfo";
 
 class Layout extends Component {
-    render() {
-         return(
-            <div className="Layout">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><a href="/">Info</a></li>
-                            <li><a href="/transit">Transit Information</a></li>
-                        </ul>
-                    </nav>
-                </header>
-                <div>
-                <Route path="/" exact component={UserForm}/>
-                <Route path="/transit" component={TransitInfo}/>
-                </div>
-        </div>
-        );
+  render() {
+    return (
+      <div className="Layout">
+        <header>
+          <nav className="navbar fixed-top navbar-expand-sm bg-dark navbar-dark">
+            <ul className="navbar-nav">
+            <span class="navbar-brand mb-0 h1">Panchanga</span>
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">
+                  Info
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/transit">
+                  Transit Information
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="content">
+          <Switch>
+            <Route exact path="/" component={UserForm} />
+            <Route path="/transit" component={TransitInfo} />
+          </Switch>
+        </main>
+      </div>
+    );
   }
-    
-    }
-
+}
 
 export default Layout;
